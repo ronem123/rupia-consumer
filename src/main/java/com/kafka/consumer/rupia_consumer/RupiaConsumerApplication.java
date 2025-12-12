3b13caea2fe9db71d4b13db4401e05e200ab3ea1
Ram Mandal <ram.mandal@8squarei.com>
2025-12-12 15:25:47 +0545

    Initialize consumer project

---
+++ src/main/java/com/kafka/consumer/rupia_consumer/RupiaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.consumer.rupia_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupiaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RupiaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/consumer/rupia_consumer/RupiaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.consumer.rupia_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RupiaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
